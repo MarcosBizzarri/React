a29aed0d7f1decccb99bc9d0d0d8851fbfdd9b2d
MarcosBizzarri <marcos-bizzarri@hotmail.com>
2023-07-04 19:55:46 -0300

    atualizando exercicios react

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 import City from './assets/city.jpeg';
 import ManageData from './componentes/ManageData';
 import ListRender from './componentes/ListRender';
+import ConditionalRender from './componentes/ConditionalRender';
+import ShowUserName from './componentes/ShowUserName';
+import { useState } from 'react';
+import CarDetails from './componentes/CarDetails';
+import Fragment from './componentes/Fragment';
+import Container from './componentes/Container';
+import ExecuteFunction from './componentes/ExecuteFunction';
+
+import ChangeMessageState from './componentes/ChangeMessageState';
+
 
 function App() {
+  // const name = "Matheus"
+  const [userName] = useState("Maria")
+
+  const cars = [
+    {id: 1, brand: "Ferrari", color: "Amarela", newCar: true, km: 0},
+    {id: 2, brand: "Honda", color: "Preto", newCar: true, km: 0},
+    {id: 3, brand: "GM", color: "Branco", newCar: false, km: 30201},
+  ]
+
+  function showMessage() {
+    console.log("Evento do componente pai!");
+  }  
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  };
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -19,6 +47,38 @@ function App() {
       </div>
       <ManageData />
       <ListRender />
+      <ConditionalRender />
+      {/* props */}
+      <ShowUserName name={userName} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul" newCar={false}/>
+      <CarDetails brande="Ford" color="Vermelha" km={0} newCar={true}/>
+      <CarDetails brade="Fiat" color="Branco" km={4500} newCar={false}/>
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails 
+        key={car.id}
+        brand={car.brand} 
+        color={car.color}
+        km={car.km} 
+        newCar={car.newCar} />
+      ))}
+       {/* fragment */}
+      <Fragment propFragment="teste" />
+      {/* children */}
+      <Container myValue="testing">
+         <p>E este é o conteúdo</p>
+      </Container>
+      <Container myValue="testing 2 ">
+         <h2>Testando container</h2>
+      </Container>
+      <Container myValue="testing 3">
+         <h3>Conteainer ok</h3>
+      </Container>
+      {/* executar funçã0 */}
+      <ExecuteFunction myFunction={showMessage}/>
+      <Message msg={message} />
+      <ChangeMessageState />
     </div>
   );
 }
