57852c1c75bba0467bc99bd42e339b7172da2697
MarcosBizzarri <marcos-bizzarri@hotmail.com>
2023-07-06 19:56:31 -0300

    atualizando react

---
+++ 4_CSS_REACT/css/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MyComponent from './Componentes/MyComponent';
+
+
+function App() {
+  return (
+  <div ClassName="App">
+    <h1>React com CSS</h1>
+     { /*CSS de componente */ }
+    <MyComponent />
+    <p>Este parágrafo é do App.js</p>
+  </div>
+  )
+  
+
+}
+
+export default App;
